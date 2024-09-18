8ce1fa0cbe4a24ab0683feb420be7a9a456c5e5f
Roland <roli.dabrowski@gmail.com>
2024-09-18 14:54:51 +0200

    added product card view

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,30 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("9a16d4c7-609b-46ec-b25a-92d72bacde77");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div>
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList
+              title="Featured Products"
+              items={products}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;

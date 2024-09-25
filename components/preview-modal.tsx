c0188d2cf545edddfc352f213375bd92f0a84dbe
Roland <roli.dabrowski@gmail.com>
2024-09-25 23:20:51 +0200

    added style and revalidate to product page

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -23,7 +23,7 @@ const PreviewModal = () => {
           <div className="sm:col-span-4 lg:col-span-5">
             <Gallery images={product.images} />
           </div>
-          <div>
+          <div className="sm:col-span-8 lg:col-span-7">
             <Info data={product} />
           </div>
         </div>
